fa000b064efa974b3c7c431173dae422acfd9b43
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2017-05-27 00:17:06 +0200

    A lot of code cleanup

--- cdi_rtcomm.h
+++ cdi_rtcomm.h
@@ -21,46 +21,6 @@ struct __attribute__((packed)) rtcomm_stats
 	uint32_t                    skipped_err;
 };
 
-struct __attribute__((packed)) rtcomm_header_data
-{
-    uint32_t					data_size;
-    uint32_t                    channels;
-    uint32_t					frame;
-    uint32_t					magic;
-    uint32_t					crc;
-    struct rtcomm_stats			stats;
-};
-
-/*
- * Data channel
- *
- * Size: 20 bytes
- *
- * Attributes:
- * - elements: how many elements are in the array
- * - addr_mul: address multiplier, usually defined by the element size in bytes
- * - addr_offset: start of array
- * - bit_mul: bit multiplier, usually defined by how many bits are in the element
- * - bit_offset: first bit in element that holds the required data
- * - mask: number of bits in element
- * - content: defined what is the data type:
- *   - 0 - unsigned integer, size of integer depends on mask attribute
- *   - 1 - signed integer, size of integer depends on mask attribute
- *   - 2 - float number, as defined by Cortex-M MCU hardware
- *   - 3 - character, this will be interpreted as string
- */
-struct __attribute__((packed)) rtcomm_channel_data
-{
-    uint16_t                    elements;
-    uint16_t                    addr_offset;
-    uint8_t                     addr_mul;
-    uint8_t                     bit_offset;
-    uint8_t                     bit_mul;
-    uint8_t                     mask;
-    uint8_t                     content;
-    char                        name[11];
-};
-
 /*
  * To each RTCOMM buffer the structure rtcomm_header is prepended to the data.
  *
@@ -68,91 +28,45 @@ struct __attribute__((packed)) rtcomm_channel_data
  */
 struct __attribute__((packed)) rtcomm_header
 {
-	union rtcomm_header_extender
-	{
-		struct rtcomm_header_data   header;
-		uint8_t						_h_padding[64];
-	}							h;
-    union rtcomm_channel_extender
-    {
-        struct rtcomm_channel_data  channel[99];
-        uint8_t                     _c_padding[1984];
-    }                           c;
+    uint32_t					data_size;
+    uint32_t					frame;
+    uint32_t					magic;
+    uint32_t					crc;
+    struct rtcomm_stats			stats;
 };
-
-uint32_t rtcomm_header_magic(void);
-
-static inline
-bool rtcomm_assert_header(void)
-{
-    struct rtcomm_header * hdr = NULL;
-
-    if (sizeof(hdr->h.header) > sizeof(hdr->h._h_padding)) {
-        return (false);
-    }
-
-    if (sizeof(hdr->c.channel) > sizeof(hdr->c._c_padding)) {
-        return (false);
-    }
-
-    return (true);
-}
 
 static inline
 void rtcomm_header_init(struct rtcomm_header * hdr)
 {
-    hdr->h.header.channels = 0;
-    hdr->h.header.data_size = 0;
+    hdr->data_size = 0;
 }
 
 static inline
 void rtcomm_header_pack(struct rtcomm_header * hdr, uint32_t data_size,
-        uint32_t channels, uint32_t frame, const struct rtcomm_stats * stats)
+        uint32_t frame, const struct rtcomm_stats * stats)
 {
-    hdr->h.header.data_size = data_size;
-    hdr->h.header.channels = channels;
-    hdr->h.header.frame = frame;
-    hdr->h.header.magic = RTCOMM_HEADER_MAGIC;
-    hdr->h.header.crc = 0;
-    hdr->h.header.stats = *stats;
+    hdr->data_size = data_size;
+    hdr->frame = frame;
+    hdr->magic = RTCOMM_HEADER_MAGIC;
+    hdr->crc = 0;
+    hdr->stats = *stats;
 }
 
 static inline
 bool rtcomm_header_unpack(struct rtcomm_header * hdr, uint32_t * data_size,
-		uint32_t * channels, uint32_t * frame)
+		uint32_t * frame)
 {
-    if (hdr->h.header.magic != RTCOMM_HEADER_MAGIC) {
+    if (hdr->magic != RTCOMM_HEADER_MAGIC) {
         return (false);
     }
 
-    if (hdr->h.header.crc != 0) {
+    if (hdr->crc != 0) {
         return (false);
     }
-    *data_size = hdr->h.header.data_size;
-    *channels = hdr->h.header.channels;
-    *frame = hdr->h.header.frame;
+    *data_size = hdr->data_size;
+    *frame = hdr->frame;
 
     return (true);
-}
-
-static inline
-void rtcomm_header_define_channel(struct rtcomm_header * hdr,
-          const struct rtcomm_channel_data * channel)
-{
-    uint32_t index = 0;
-
-    while (channel->name[0] != 0) {
-        memcpy(&hdr->c.channel[index], channel, sizeof(*channel));
-        index++;
-        channel++;
-    }
-}
-
-static inline
-void rtcomm_header_mod_elements(struct rtcomm_header * hdr, uint32_t index,
-		uint32_t elements)
-{
-    hdr->c.channel[index].elements = (uint16_t)elements;
 }
 
 #endif /* APPLICATION_INCLUDE_CDI_CDI_RTCOMM_H_ */
--- io.h
+++ io.h
@@ -5,126 +5,36 @@
  *      Author: nenad
  */
 
-#ifndef APPLICATION_INCLUDE_ACQUNITY_COMMANDS_H_
-#define APPLICATION_INCLUDE_ACQUNITY_COMMANDS_H_
+#ifndef CDI_IO_H_
+#define CDI_IO_H_
 
 #include <stdint.h>
 
 #include "cdi_rtcomm.h"
 
-/*
- *  MSB                         LSB
- * +---+---+---+---+---+---+---+---+
- * | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
- * +---+---+---+---+---+---+---+---+
- *  |   |   |   |   |   |   |   |
- *  |   |   |   |   |   |   |   +---- CMD
- *  |   |   |   |   |   |   +-------- PAR 0
- *  |   |   |   |   |   +------------ PAR 1
- *  |   |   |   |   +---------------- PAR 2
- *  |   |   |   +-------------------- PAR 3
- *  |   |   +------------------------ PAR 4
- *  |   +---------------------------- PAR 5
- *  +-------------------------------- PAR 6
- */
+#define IO_ID							0x26
+#define IO_BUFF_SIZE              		3000
+#define IO_AUX_CHANNELS           		2
+#define IO_PROBE_CHANNELS           	3
 
-#define ACQUNITY_ID						0x26
-#define ACQUNITY_BUFF_SIZE              2048
-#define ACQUNITY_AUX_CHANNELS           4
-#define ACQUNITY_ACQ_CHANNELS           3
+#define IO_ADC_SIGNED_MAX				8388607
+#define IO_ADC_SIGNED_MIN				-8388608
+#define IO_ADC_UNSIGNED_MAX				16777215u
+#define IO_ADC_UNSIGNED_MIN				0u
+#define IO_ADC_FS                       (IO_ADC_UNSIGNED_MAX + 1u)
 
-#define ACQ_ADC_SIGNED_MAX				8388607
-#define ACQ_ADC_SIGNED_MIN				-8388608
-#define ACQ_ADC_UNSIGNED_MAX			16777215u
-#define ACQ_ADC_UNSIGNED_MIN			0u
-#define ACQ_ADC_FS                      (ACQ_ADC_UNSIGNED_MAX + 1u)
+#define IO_CHANNEL_X              		0
+#define IO_CHANNEL_Y              		1
+#define IO_CHANNEL_Z              		2
 
-#define ACQ_CHANNEL_X              		0
-#define ACQ_CHANNEL_Y              		1
-#define ACQ_CHANNEL_Z              		2
+#define IO_CHANNEL_X_MASK               (0x1u << 0)
+#define IO_CHANNEL_Y_MASK               (0x1u << 1)
+#define IO_CHANNEL_Z_MASK               (0x1u << 2)
+#define IO_CHANNEL_XYZ_MASK				\
+	(IO_CHANNEL_X_MASK | IO_CHANNEL_Y_MASK | IO_CHANNEL_Z_MASK)
 
-#define ADS_REG_STATUS                  0x00
-#define ADS_REG_MUX                     0x01
-#define ADS_REG_ADCON                   0x02
-#define ADS_REG_DRATE                   0x03
-#define ADS_REG_IO                      0x04
-#define ADS_REG_OFC0                    0x05
-#define ADS_REG_OFC1                    0x06
-#define ADS_REG_OFC2                    0x07
-#define ADS_REG_FSC0                    0x08
-#define ADS_REG_FSC1                    0x09
-#define ADS_REG_FSC2                    0x0a
-
-#define ADS_STATUS_ORDER_LSB            (0x1u << 3)
-#define ADS_STATUS_ACAL_ENABLED         (0x1u << 2)
-#define ADS_STATUS_BUFEN_ENABLED        (0x1u << 1)
-
-#define ADS_MUX(pos, neg)               ((uint8_t)((uint8_t)((pos & 0xfu) << 4u) | (uint8_t)(neg & 0xfu)))
-
-#define ADS_ADCON_CLKOUT_FCLKIN         (0x1u << 5)
-#define ADS_ADCON_CLKOUT_FCLKIN_DIV_2   (0x2u << 5)
-#define ADS_ADCON_CLKOUT_FLCKIN_DIV_4   (0x3u << 5)
-#define ADS_ADCON_SDCS_05UA             (0x1u << 3)
-#define ADS_ADCON_SDCS_2UA              (0x2u << 3)
-#define ADS_ADCON_SDCS_10UA             (0x3u << 3)
-#define ADS_ADCON_PGA_2                 (0x1u << 0)
-#define ADS_ADCON_PGA_4                 (0x2u << 0)
-#define ADS_ADCON_PGA_8                 (0x3u << 0)
-#define ADS_ADCON_PGA_16                (0x4u << 0)
-#define ADS_ADCON_PGA_32                (0x5u << 0)
-#define ADS_ADCON_PGA_64                (0x6u << 0)
-
-#define ADS_DRATE_30KSPS                0xf0
-#define ADS_DRATE_15KSPS                0xe0
-#define ADS_DRATE_7500SPS               0xd0
-#define ADS_DRATE_3750SPS               0xc0
-#define ADS_DRATE_2000SPS               0xb0
-#define ADS_DRATE_1000SPS               0xa1
-#define ADS_DRATE_500SPS                0x92
-#define ADS_DRATE_100SPS                0x82
-#define ADS_DRATE_60SPS                 0x72
-#define ADS_DRATE_50SPS                 0x63
-#define ADS_DRATE_30SPS                 0x53
-#define ADS_DRATE_25SPS                 0x43
-#define ADS_DRATE_15SPS                 0x33
-#define ADS_DRATE_10SPS                 0x23
-#define ADS_DRATE_5SPS                  0x13
-#define ADS_DRATE_2_5SPS                0x03
-
-#define ADS_IO_DIR3                     0x80
-#define ADS_IO_DIR2                     0x40
-#define ADS_IO_DIR1                     0x20
-#define ADS_IO_DIR0                     0x10
-
-#define ADS_CMD_WAKEUP                  0x00
-#define ADS_CMD_RDATA                   0x01
-#define ADS_CMD_RDATAC                  0x03
-#define ADS_CMD_SDATAC                  0x0f
-#define ADS_CMD_RREG                    0x10
-#define ADS_CMD_WREG                    0x50
-#define ADS_CMD_SELFCAL                 0xf0
-#define ADS_CMD_SELFOCAL                0xf1
-#define ADS_CMD_SELFGCAL                0xf2
-#define ADS_CMD_SYSOCAL                 0xf3
-#define ADS_CMD_SYSGCAL                 0xf4
-#define ADS_CMD_SYNC                    0xfc
-#define ADS_CMD_STANDBY                 0xfd
-#define ADS_CMD_RESET                   0xfe
-
-#define ADS_CLOCK_SLAVE                 0x1u
-
-#define ACQ_CHANNEL_X_MASK              (0x1u << 0)
-#define ACQ_CHANNEL_Y_MASK              (0x1u << 1)
-#define ACQ_CHANNEL_Z_MASK              (0x1u << 2)
-#define ACQ_CHANNEL_XYZ_MASK			(ACQ_CHANNEL_X_MASK | ACQ_CHANNEL_Y_MASK | ACQ_CHANNEL_Z_MASK)
-
-/*--  ACQUNITY command: CMD_SET_SYSTEM_CFG  ----------------------------------*/
-
-#define CMD_SET_SYSTEM_CFG				0x01
-#define CMD_SET_SYSTEM_STOP             0x02
-#define ACQ_CFG_SIZE_TO_ENUM(size)      ((size + 15u) / 16u)
-
-/* Trigger signal is output on a pin
+/* IO_TRIG_MODE_OUT
+ * Trigger signal is output on a pin
  * There are two available modes depending on the sample rate speed:
  *  - if sample speed is low (less than 15kSPS) then trigger rising edge is
  *  	generated just at the beginning of the sample read. After calculated
@@ -132,175 +42,32 @@
  *  - if sample speed is high (15-30 kSPS) then the rising edge is generated at
  *  	start of reading and the falling edge is generated at end of reading of
  *  	all bytes from ADCs.
+ *
+ * IO_TRIG_MODE_IN_CONTINUOUS
+ * This trigger means that after each trigger a sample is taken.
+ *
+ * IO_TRIG_MODE_IN_SINGLE_SHOT
+ * This trigger triggers only once and then it will continue on its own.
  */
-#define TRIG_MODE_OUT          			0
+#define IO_TRIG_MODE_OUT          		0
+#define IO_TRIG_MODE_IN_CONTINUOUS   	1
+#define IO_TRIG_MODE_IN_SINGLE_SHOT    	2
 
-/* This trigger means that after each trigger a sample is taken.
+/* IO_ADC_MODE_CONTINUOUS
+ * ADC is sampling automatically at specified speed.
+ *
+ * IO_ADC_MODE_REQUEST
+ * ADC is sampling only when requested by MCU.
  */
-#define TRIG_MODE_IN_CONTINUOUS   		1
+#define IO_ADC_MODE_CONTINUOUS			0
+#define IO_ADC_MODE_REQUEST				1
 
-/* This trigger triggers only once and then it will continue on its own.
- */
-#define TRIG_MODE_IN_SINGLE_SHOT      	2
-
-#define ACQ_MODE_CONTINUOUS				0
-#define ACQ_MODE_REQUEST				1
-
-
-
-struct __attribute__((packed)) acq_sample
-{
-	union acq_sample_data
-	{
-		int32_t						idata;
-		float						fdata;
-	}							_channel[3];
-    uint32_t                    _metadata;
-};
-
-struct __attribute__((packed)) acq_buffer
+struct __attribute__((packed)) io_buffer
 {
     struct rtcomm_header        header;
-    int32_t                     sample[3000][ACQUNITY_ACQ_CHANNELS];
-    uint32_t                    aux[ACQUNITY_AUX_CHANNELS];
+    int32_t           			sample[IO_BUFF_SIZE][IO_PROBE_CHANNELS];
+    uint8_t						gain[IO_BUFF_SIZE][IO_PROBE_CHANNELS];
+    uint32_t                    aux[IO_AUX_CHANNELS];
 };
 
-
-/*--  ACQUNITY command: CMD_GET_DEV_MEM  -------------------------------------*/
-/*
- * No parameters
- */
-#define CMD_GET_DEV_MEM                 0xf0
-
-/*
- * METADATA
- *
- *  +---+---+--------------------------+
- *  | G | T |        TIMESTAMP         |
- *  +---+---+--------------------------+
- *   | | | | |                        |
- *   | | | | |                        +---- 0
- *   | | | | +----------------------------- 25
- *   | | | +------------------------------- 26
- *   | | +--------------------------------- 28
- *   | +----------------------------------- 29
- *   +------------------------------------- 31
- *
- *   G - gain [31:29] - 3 bits
- *   T - type [28:26] - 3 bits
- *    - SAMPLE_TYPE_INT
- *    - SAMPLE_TYPE_FLOAT
- *   TIMESTAMP - timestamp [25:0] - 26 bits
- */
-
-#define SAMPLE_GAIN_Pos					(0u)
-#define SAMPLE_GAIN_Mask				(0xffu << SAMPLE_GAIN_Pos)
-#define SAMPLE_TYPE_Pos					(8)
-#define SAMPLE_TYPE_Mask				(0xffu << SAMPLE_TYPE_Pos)
-
-#define SAMPLE_TYPE_INT					(0x0)
-#define SAMPLE_TYPE_FLOAT				(0x1)
-
-#if defined(__ARM_ARCH_7EM__)
-#include "base/bitop.h"
-#define io_raw_adc_to_int(raw_data)		n_ext_i24((int32_t)(raw_data))
-
-static inline
-void sample_init(struct acq_sample * sample, uint32_t raw_data, uint32_t channel)
-{
-	sample->_channel[channel].idata = io_raw_adc_to_int(raw_data);
-}
-
-#endif
-
-static inline
-void sample_cpy_metadata(struct acq_sample * dst, const struct acq_sample * src)
-{
-	dst->_metadata = src->_metadata;
-}
-
-
-
-static inline
-void sample_set_int(struct acq_sample * sample, int32_t value, uint32_t channel)
-{
-	sample->_channel[channel].idata = value;
-}
-
-
-
-static inline
-int32_t sample_get_int(const struct acq_sample * sample, uint32_t channel)
-{
-	return (sample->_channel[channel].idata);
-}
-
-
-
-static inline
-void sample_set_float(struct acq_sample * sample, float value, uint32_t channel)
-{
-	sample->_channel[channel].fdata = value;
-}
-
-
-
-static inline
-float sample_get_float(const struct acq_sample * sample, uint32_t channel)
-{
-	return (sample->_channel[channel].fdata);
-}
-
-
-
-static inline
-void sample_set_gain(struct acq_sample * sample, uint32_t gain)
-{
-	gain <<= SAMPLE_GAIN_Pos;
-    gain &= SAMPLE_GAIN_Mask;
-
-	sample->_metadata &= ~SAMPLE_GAIN_Mask;
-	sample->_metadata |= gain;
-}
-
-
-
-static inline
-uint32_t sample_get_gain(const struct acq_sample * sample)
-{
-	return (sample->_metadata >> SAMPLE_GAIN_Pos);
-}
-
-
-
-/**@brief		Set the sample type
- * @param 		sample - Pointer to acq_sample structure
- * @param 		type
- * 				- SAMPLE_TYPE_INT
- * 				- SAMPLE_TYPE_FLOAT
- */
-static inline
-void sample_set_type(struct acq_sample * sample, uint32_t type)
-{
-	type <<= SAMPLE_TYPE_Pos;
-	type &= SAMPLE_TYPE_Mask;
-
-	sample->_metadata &= ~SAMPLE_TYPE_Mask;
-	sample->_metadata |= type;
-}
-
-
-
-/**@brief		Get the sample type
- * @param 		sample - Pointer to acq_sample structure
- * @return
- *  @retval		SAMPLE_TYPE_INT
- *  @retval		SAMPLE_TYPE_FLOAT
- */
-static inline
-uint32_t sample_get_type(const struct acq_sample * sample)
-{
-	return ((sample->_metadata & SAMPLE_TYPE_Mask) >> SAMPLE_TYPE_Pos);
-}
-
-#endif /* APPLICATION_INCLUDE_ACQUNITY_COMMANDS_H_ */
+#endif /* CDI_IO_H_ */
